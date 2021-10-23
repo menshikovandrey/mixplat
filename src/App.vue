db6c3fae90a9051c9da5853a2667df7121031e83
andreymenshikov <menshikov.andrey@mail.ru>
2021-10-23 23:30:12 +0300

    Refactor: Перенос функционала Select.vue в App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,36 @@
 <template>
   <div id="app">
-    <Header/>
-    <Select/>
-    <Card/>
-    <Mobile/>
-    <Apple/>
-    <Footer/>
+
+    <Header/> <!-- Заголовок -->
+
+    <!-- Выпадающий список -->
+    <div id="selectContainer" class="row">
+      <div class="col-lg-4 mx-auto">
+        <div class="form-group row">
+          <label class="col-lg-4 col-form-label">Способ оплаты</label>
+          <div class="col-lg-8">
+            <select v-model="selected"
+                    @change="onChange()"
+                    class="custom-select form-control">
+              <option v-for="option in this.selectOptions" v-bind:key="option.value" v-bind:value="option.value">
+                {{ option.text }}
+              </option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <Card/>   <!-- Банковская карта -->
+    <Mobile/> <!-- С баланса мобильного -->
+    <Apple/>  <!-- Apple Pay -->
+
+    <Footer/> <!-- Copyright снизу -->
   </div>
 </template>
 
 <script>
 import Header from "./components/Header";
-import Select from "./components/Select";
 import Card from './components/Card';
 import Mobile from "./components/Mobile";
 import Apple from "./components/Apple";
@@ -19,7 +38,45 @@ import Footer from "./components/Footer";
 
 export default {
   name: 'App',
-  components: {Header, Select, Card, Mobile, Apple, Footer},
+  components: {Header, Card, Mobile, Apple, Footer},
+  data() {
+    return {
+      selected: 'card', // Выпадающий список - значение по умолчанию
+      selectOptions: [  // Выпадающий список - массив
+        {value: 'card', text: 'Банковская карта'},
+        {value: 'mobile', text: 'С баланса мобильного'},
+        {value: 'apple', text: 'Apple Pay'},
+      ],
+    }
+  },
+  methods: {
+    /**
+     * Выполняется при изменеии значения в выпадабщем списке
+     */
+    onChange() {
+      let selected = this.selected; // получаем способ оплаты
+      this.hideScreens();           // скрыть все способы оплаты
+      switch (selected) {           // отобразить выбранный из выпадающего списка
+        case 'card':
+          document.getElementById('card').style.display = "flex";
+          break;
+        case 'mobile':
+          document.getElementById('mobile').style.display = "flex";
+          break;
+        case 'apple':
+          document.getElementById('apple').style.display = "flex";
+          break;
+      }
+    },
+    /**
+     * Скрыть все способы оплаты
+     */
+    hideScreens() {
+      document.getElementById('card').style.display = "none";
+      document.getElementById('mobile').style.display = "none";
+      document.getElementById('apple').style.display = "none";
+    }
+  }
 }
 </script>
 
@@ -35,5 +92,19 @@ export default {
   overflow-x: hidden;
   min-height: 100%;
   position: relative;
+}
+
+#selectContainer {
+  margin-top: 30px;
+}
+
+.col-form-label,
+.custom-select {
+  text-align: left;
+  font-family: Open Sans;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 13px;
+  color: #333333;
 }
 </style>
